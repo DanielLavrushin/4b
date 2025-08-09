24567fbc472cb8f8b311722b918a34c461b17973
Daniel Lavrushin <lavrushin@gmail.com>
2025-08-09 22:16:31 +0200

    fix: Update installation script to use tar.gz files and improve process flow

--- b4.sh
+++ b4.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 
-B4_SHARE_DIR="/opt/etc/share/b4"
+ENABLED=yes
+PROCS=b4
+PATH=/opt/sbin:/opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+ARGS="--threads 4 --sni-domains-file ./domains.txt"
+B4_SHARE_DIR="/opt/share/b4"
 
-B4_LATEST_RELEASE_URL="https://github.com/DanielLavrushin/asuswrt-merlin-xrayui/releases/latest/download/"
+B4_LATEST_RELEASE_URL="https://github.com/daniellavrushin/b4/releases/latest/download/"
 
 install() {
+    echo "Installing B4..."
     mkdir -p $B4_SHARE_DIR
 
     local arch=$(uname -m)
@@ -12,28 +17,28 @@ install() {
     local asset_name=""
     case "$arch" in
     x86_64)
-        asset_name="b4-linux-64.zip"
+        asset_name="b4-linux-64.tar.gz"
         ;;
     i686 | i386)
-        asset_name="b4-linux-386.zip"
+        asset_name="b4-linux-386.tar.gz"
         ;;
     armv5* | armv6* | armv7*)
-        asset_name="b4-linux-arm5.zip"
+        asset_name="b4-linux-arm5.tar.gz"
         ;;
     aarch64 | arm64)
-        asset_name="b4-linux-arm64.zip"
+        asset_name="b4-linux-arm64.tar.gz"
         ;;
     mips)
-        asset_name="b4-linux-mips.zip"
+        asset_name="b4-linux-mips.tar.gz"
         ;;
     mipsle)
-        asset_name="b4-linux-mipsle.zip"
+        asset_name="b4-linux-mipsle.tar.gz"
         ;;
     mips64)
-        asset_name="b4-linux-mips64.zip"
+        asset_name="b4-linux-mips64.tar.gz"
         ;;
     mips64le)
-        asset_name="b4-linux-mips64le.zip"
+        asset_name="b4-linux-mips64le.tar.gz"
         ;;
     *)
         echo "Unsupported architecture: $arch"
@@ -41,12 +46,31 @@ install() {
         ;;
     esac
 
-    local tmp_zip="/tmp/b4.zip"
+    local tmp_zip="/tmp/b4.tar.gz"
     rm -rf "$tmp_zip"
 
     local asset_url="$B4_LATEST_RELEASE_URL$asset_name"
+    echo "Downloading $asset_url..."
     curl -L "$asset_url" -o "$tmp_zip"
-    unzip -o "$tmp_zip" -d "$B4_SHARE_DIR"
+    tar -xzf "$tmp_zip" -C "$B4_SHARE_DIR"
+    rm -f "$tmp_zip"
+    chmod +x "$B4_SHARE_DIR/b4"
+}
+
+start() {
+    echo "Starting B4..."
+    "$B4_SHARE_DIR/b4" &
+}
+
+stop() {
+    echo "Stopping B4..."
+    killall b4
+}
+
+restart() {
+    echo "Restarting B4..."
+    stop
+    start
 }
 
 firewall_start_v6() {
@@ -88,3 +112,57 @@ firewall_stop_v4() {
     _iptables iptables -D OUTPUT -m mark --mark 32768/32768 -j ACCEPT
     iptables -t mangle -X B4 >/dev/null 2>&1
 }
+
+system_config() {
+    sysctl -w net.netfilter.nf_conntrack_checksum=0 >/dev/null 2>&1
+    sysctl -w net.netfilter.nf_conntrack_tcp_be_liberal=1 >/dev/null 2>&1
+}
+
+_iptables() {
+    ARG="$@"
+    CMD=$1    # iptables or ip6tables
+    ACTION=$2 # -I, -A, -D
+    shift
+    shift
+    RULE="$@"
+
+    $CMD -C $RULE 2>/dev/null
+    exists=$((!$?))
+
+    if [ "$ACTION" = "-A" -o "$ACTION" = "-I" ]; then
+        if [ $exists -eq 0 ]; then
+            $ARG || exit 1
+        fi
+    else # -D
+        if [ $exists -ne 0 ]; then
+            $ARG
+        fi
+    fi
+}
+
+case "$1" in
+install)
+    install
+    ;;
+start)
+    start
+    ;;
+stop)
+    stop
+    ;;
+restart)
+    restart
+    ;;
+firewall_start)
+    firewall_start_v4
+    firewall_start_v6
+    ;;
+firewall_stop)
+    firewall_stop_v4
+    firewall_stop_v6
+    ;;
+*)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+esac
